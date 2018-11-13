9df1684443916fd3ac8b5bf8cddeb410d85d449b
pigeon <>
2018-11-13 11:18:06 -0500

    template for alamofire/swiftyjson projects

---
+++ AFStarter/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  AFStarter
+//
+//  Created by parrot on 2018-11-13.
+//  Copyright © 2018 room1. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import SwiftyJSON
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
